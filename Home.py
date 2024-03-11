2f2d4c930806bd4754c92dc9e6219f9e55bb3fb7
Sunny Lim <limhss@gmail.com>
2024-03-11 18:13:14 +0800

    A

--- Home.py
+++ Home.py
@@ -35,6 +35,9 @@ with st.sidebar:
     # Add login/logout buttons
     if st.session_state["authenticated"]:
         authenticate.button_logout()
+        user_info = authenticate.get_user_info(st.session_state['access_token'])
+        st.write(type(user_info))
+        st.write(user_info)
         if st.button("1_📈_Plotting_Demo"):
             st.switch_page("pages/Plotting_Demo.py")
 
