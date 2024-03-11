17600fc3ddb5c96a275f2ef92f473189f5ccbc44
Sunny Lim <limhss@gmail.com>
2024-03-11 18:04:39 +0800

    A

--- components/authenticate.py
+++ components/authenticate.py
@@ -204,6 +204,7 @@ def set_st_state_vars():
         st.session_state["auth_code"] = auth_code
         st.session_state["authenticated"] = True
         st.session_state["user_cognito_groups"] = user_cognito_groups
+        st.session_state["access_token"] = access_token
 
 # -----------------------------
 # Login/ Logout HTML components
